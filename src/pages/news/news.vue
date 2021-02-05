3fefb2d2eb7129eb39365760c31823aaa6728316
jy_lijun <web_devil@sina.com>
2021-02-05 16:31:46 +0800

    init<global>: 初始化提交
    添加eslint，stylelint，ts，sass，husky等
    vuex使用vuex-class形式

---
+++ src/pages/news/news.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="content">
+    <view>
+      <text class="title">{{ title + stateCount }}</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { State, Action, Mutation } from "vuex-class";
+
+@Component({})
+export default class News extends Vue {
+  title = "abc";
+  @State("msg") stateCount: any;
+}
+</script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .title {
+    font-size: 36rpx;
+    color: #f00;
+  }
+}
+</style>
